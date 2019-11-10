bf831e7fc8de29968c3d62927874ac94c66907c1
Keller Clayderman <keller_clayderman@hotmail.com>
2019-11-10 15:46:22 -0300

    Improve and comment api examples

--- api/profiles/[name]/[address].js
+++ api/profiles/[name]/[address].js
@@ -1,7 +1,14 @@
 const db = require('../../../lib/db')
 const escape = require('sql-template-strings')
 
+// Exemplo de api
+// Acessada como api/profiles/NOME/ENDERECO
+// Nesse exemplo, query.name e query.address vem do nome do arquivo e pasta [name] e [address]
+// que fazem com que se possa formatar a query entre barras em vez de usar a query string
+// A resposta dada aqui é um json com o profile encontrado ou um json vazio caso não exista
+
 module.exports = async (req, res) => {
+  // [profile] é um açucar sintático para pegar o primeiro elemento da lista retornada e atribuir a var profile
   const [profile] = await db.query(escape`
     SELECT *
     FROM profiles
