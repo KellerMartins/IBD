085ec89e38c92e0c869d59f5d408e5696e8a402b
Keller Clayderman <keller.clayderman.m@gmail.com>
2019-11-21 13:38:52 -0300

    Change the uf api on backend

--- api/coordenadas/ufs/index.js
+++ api/coordenadas/ufs/index.js
@@ -3,17 +3,16 @@ const escape = require('sql-template-strings')
 
 module.exports = async (req, res) => {
   
-  const coords = await db.query(escape`
+  const ufs = await db.query(escape`
       SELECT AVG(longitude_coord) as longitude, 
              AVG(latitude_coord) as latitude, 
              AVG(altitude_coord) as altitude, 
              AVG(tamanho_coord) as tamanho,
-             cod_uf as cod
-      FROM municipio NATURAL JOIN coord
+             sigla_uf as sigla
+      FROM municipio NATURAL JOIN coord NATURAL JOIN uf
       GROUP BY cod_uf
       ORDER BY cod_uf
     `)
 
-  var ufs = coords.map((o) => {o.tamanho = Math.min(o.tamanho*10, 75); return o})
   res.status(200).json({ ufs })
 }
