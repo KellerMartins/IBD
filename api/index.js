e306c2edf1a085e454549e3c9acd170674e4de65
Keller Clayderman Martins de Oliveira <kellermartins@piracicaba.grad.dcc.ufmg.br>
2019-12-02 18:14:43 -0300

    Arange queries in groups

--- api/index.js
+++ api/index.js
@@ -5,13 +5,27 @@ module.exports = async (req, res) => {
       title: "População",
       icon: "mdi-account-group",
       queries: [
-        {id:"genero", title: "Gênero", icon: "mdi-gender-male-female", chart:"pie"},
-        {id:"renda", title: "Renda", icon: "mdi-cash-multiple", chart:"pie"},
-        {id:"emprego", title: "Emprego", icon: "mdi-worker", chart:"pie"},
-        {id:"moradia", title: "Moradia", icon: "mdi-home", chart:"pie"},
-        {id:"saneamento", title: "Saneamento Básico",icon: "mdi-paper-roll", chart:"pie"},
-        {id:"escolaridade", title: "Escolaridade", icon: "mdi-school", chart:"pie"},
-        {id:"natalidade", title: "Natalidade", icon: "mdi-baby-carriage", chart:"pie"},
+        {id:"relatorio/numhabitantes", title: "Nº de habitantes", icon: "mdi-account-group", chart:"pie"},
+        {id:"relatorio/popruralpecuaria", title: "População X pecuária", icon: "mdi-barn", chart:"pie"},
+        {id:"relatorio/empregoformal", title: "Emprego formal", icon: "mdi-worker", chart:"bar"},
+      ],
+    },
+
+    "education": {
+      title: "Educação",
+      icon: "mdi-school",
+      queries: [
+        {id:"relatorio/alfabetizacao", title: "Alfabetização", icon: "mdi-alphabetical", chart:"pie"},
+        {id:"relatorio/mulheresdoutoras", title: "Mulheres com doutorado", icon: "mdi-face-woman", chart:"pie"},
+      ],
+    },
+
+    "households": {
+      title: "Domicílios",
+      icon: "mdi-home",
+      queries: [
+        {id:"relatorio/porcentsemagua", title: "Munic. sem água encanada", icon: "mdi-water-pump", chart:"pie"},
+        {id:"relatorio/urbanorural", title: "Domicílios urbanos/rurais", icon: "mdi-home-city", chart:"pie"},
       ],
     },
 
@@ -19,7 +33,9 @@ module.exports = async (req, res) => {
       title: "Economia",
       icon: "mdi-currency-usd",
       queries: [
-        {id:"teste", title: "Teste", icon: "mdi-cash-multiple", chart:"pie"},
+        {id:"relatorio/pibmedio", title: "PIB médio por município", icon: "mdi-currency-usd", chart:"line"},
+        {id:"relatorio/pibmedioprimaria", title: "PIB médio X setor primario", icon: "mdi-sprout", chart:"pie"},
+        {id:"relatorio/pibmediograduado", title: "PIB médio X graduação", icon: "mdi-school", chart:"line"},
       ],
     },
 
@@ -28,10 +44,10 @@ module.exports = async (req, res) => {
       icon: "mdi-file-chart",
       queries: [
         {id:"relatorio/numhabitantes", title: "Nº de habitantes", icon: "mdi-account-group", chart:"pie"},
-        {id:"saneamento/porcentsemagua", title: "Munic. sem água encanada", icon: "mdi-water-pump", chart:"pie"},
+        {id:"relatorio/porcentsemagua", title: "Munic. sem água encanada", icon: "mdi-water-pump", chart:"pie"},
         {id:"relatorio/urbanorural", title: "Domicílios urbanos/rurais", icon: "mdi-home-city", chart:"pie"},
         {id:"relatorio/mulheresdoutoras", title: "Mulheres com doutorado", icon: "mdi-face-woman", chart:"pie"},
-        {id:"educacao/alfabetizacao", title: "Alfabetização", icon: "mdi-alphabetical", chart:"pie"},
+        {id:"relatorio/alfabetizacao", title: "Alfabetização", icon: "mdi-alphabetical", chart:"pie"},
         {id:"relatorio/pibmedio", title: "PIB médio por município", icon: "mdi-currency-usd", chart:"line"},
         {id:"relatorio/pibmedioprimaria", title: "PIB médio X setor primario", icon: "mdi-sprout", chart:"pie"},
         {id:"relatorio/pibmediograduado", title: "PIB médio X graduação", icon: "mdi-school", chart:"line"},
