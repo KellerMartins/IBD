928164fc51d695db07fa4c72a93771b2b1ff11bb
Keller Clayderman Martins de Oliveira <kellermartins@doce.grad.dcc.ufmg.br>
2019-12-03 16:05:19 -0300

    Improve description of classe query

--- api/pais/populacao/classe/index.js
+++ api/pais/populacao/classe/index.js
@@ -3,11 +3,11 @@ const escape = require('sql-template-strings')
 
 module.exports = async (req, res) => {
   const classe = await db.query(escape`
-    SELECT SUM(num_classe_A) as A, 
-           SUM(num_classe_B) as B, 
-           SUM(num_classe_C) as C, 
-           SUM(num_classe_D) as D, 
-           SUM(num_classe_E) as E
+    SELECT SUM(num_classe_A) as A20_inf, 
+           SUM(num_classe_B) as B10_20, 
+           SUM(num_classe_C) as C4_10, 
+           SUM(num_classe_D) as D2_4, 
+           SUM(num_classe_E) as E0_2
     FROM populacao
   `)
 
@@ -15,7 +15,9 @@ module.exports = async (req, res) => {
     return res.status(500).end(classe.error.message);
 
   var total = {}
-  for (let [k, v] of Object.entries(classe[0]))
-    total["Pessoas na classe "+k] = Math.round(v)
+  for (let [k, v] of Object.entries(classe[0])) {
+    let numwages = k.substr(1).split('_')
+    total["Classe "+k[0]+" ("+numwages[0]+"-"+numwages[1]+" salários mínimos)"] = Math.round(v)
+  }
   res.status(200).json(total)
 }
--- api/regiao/[coordenadas]/populacao/classe/index.js
+++ api/regiao/[coordenadas]/populacao/classe/index.js
@@ -10,11 +10,11 @@ module.exports = async (req, res) => {
   else {
     let [min, max] = coords;
     const classe = await db.query(escape`
-      SELECT SUM(num_classe_A) as A, 
-             SUM(num_classe_B) as B, 
-             SUM(num_classe_C) as C, 
-             SUM(num_classe_D) as D, 
-             SUM(num_classe_E) as E
+      SELECT SUM(num_classe_A) as A20_inf, 
+             SUM(num_classe_B) as B10_20, 
+             SUM(num_classe_C) as C4_10, 
+             SUM(num_classe_D) as D2_4, 
+             SUM(num_classe_E) as E0_2
       FROM populacao NATURAL JOIN municipio NATURAL JOIN coord
       WHERE latitude_coord  > ${min.lat} AND latitude_coord  < ${max.lat} AND
             longitude_coord > ${min.lon} AND longitude_coord < ${max.lon}
@@ -24,8 +24,10 @@ module.exports = async (req, res) => {
       return res.status(500).end(classe.error.message);
 
     var total = {}
-    for (let [k, v] of Object.entries(classe[0]))
-      total["Pessoas da classe "+k+" na região"] = Math.round(v)
+    for (let [k, v] of Object.entries(classe[0])) {
+      let numwages = k.substr(1).split('_')
+      total["Classe "+k[0]+" ("+numwages[0]+"-"+numwages[1]+" salários mínimos)"] = Math.round(v)
+    }
     res.status(200).json(total)
   }
 }
